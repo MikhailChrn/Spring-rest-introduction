5e7f8298681dac8e7db8d3267c459c70393c3890
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-02-04 19:07:09 +0400

    Add ExceptionHandler classes

---
+++ src/main/java/com/company/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.company.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
